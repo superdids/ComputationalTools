e89a6634006f9cee65f85a5b1230590bcb3cfe21
superdids <didrik.aubert@gmail.com>
2016-10-11 15:48:10 +0200

    Dummy file for week 07 published.

---
+++ src/mapreduce.py
@@ -0,0 +1,20 @@
+from mrjob.job import MRJob
+
+class MapReduce(MRJob):
+
+
+
+    def mapper(self, _, line):
+        yield "chars", len(line)
+        yield "words", len(line.split())
+        yield "lines", 1
+
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+
+if __name__ == '__main__':
+    MapReduce.run()
+
+
