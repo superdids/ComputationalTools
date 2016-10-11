0ba1e01947fc781c8386ee4e92601b13cda7b964
superdids <didrik.aubert@gmail.com>
2016-10-11 16:28:16 +0200

    Some temporary testing on first exercise.

--- src/mapreduce.py
+++ src/mapreduce.py
@@ -2,15 +2,30 @@ from mrjob.job import MRJob
 
 class MapReduce(MRJob):
 
-
-
     def mapper(self, _, line):
-        yield "chars", len(line)
-        yield "words", len(line.split())
-        yield "lines", 1
+        items = dict()
 
+        exists = lambda key: key in items
+        format_string = lambda string: ''\
+            .join(c for c in string if c not in '!"#%&/()=?\\1234567890+.,;:')\
+            .lower()
+
+        for word in line.split():
+            word = format_string(word)
+            if exists(word):
+                items[word].append(1)
+            else:
+                items[word] = [1]
+
+        #for key in items:
+            #print('k: ', key, ', v: ', items[key])
+
+            #yield key, items[key]
+
+        return ((key, items[key]) for key in items)
 
     def reducer(self, key, values):
+
         yield key, sum(values)
 
 
