ebf2b5c30b7105b2b527a00032f681e76f0832de
superdids <didrik.aubert@gmail.com>
2016-10-11 16:45:29 +0200

    E71 done, moved to another folder.

---
+++ mapreduce/exercise1.py
@@ -0,0 +1,29 @@
+from mrjob.job import MRJob
+
+class Exercise1(MRJob):
+
+    # Generates a (k,v) pair for each occurence of a word
+    # in a text file.
+    def mapper(self, _, line):
+
+        # Formats a string by omitting certain characters, then
+        # lower-cases the resulting string.
+        format_string = lambda string: '' \
+            .join(c for c in string if c not in '!"#%&/()=?\\1234567890+.,;:') \
+            .lower()
+
+        # Now we yield each occurrence of a word (along with 1 to describe
+        # 'one occurrence of this word'). This will be automatically grouped
+        # by the group step in the MapReduce paradigm.
+        for word in line.split():
+            yield format_string(word), 1
+
+    # The reduction step is now
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+
+if __name__ == '__main__':
+    Exercise1.run()
+
+
--- mapreduce/mapreduce.py
+++ mapreduce/mapreduce.py
@@ -5,27 +5,14 @@ class MapReduce(MRJob):
     def mapper(self, _, line):
         items = dict()
 
-        exists = lambda key: key in items
         format_string = lambda string: ''\
             .join(c for c in string if c not in '!"#%&/()=?\\1234567890+.,;:')\
             .lower()
 
         for word in line.split():
-            word = format_string(word)
-            if exists(word):
-                items[word].append(1)
-            else:
-                items[word] = [1]
-
-        #for key in items:
-            #print('k: ', key, ', v: ', items[key])
-
-            #yield key, items[key]
-
-        return ((key, items[key]) for key in items)
+            yield format_string(word), 1
 
     def reducer(self, key, values):
-
         yield key, sum(values)
 
 
