ff88828699ae06b9429b1f75c88473e53cf3c570
superdids <didrik.aubert@gmail.com>
2016-10-13 17:59:21 +0200

    7.3 working, finishing documentation.

--- mapreduce/mapreduce.py
+++ mapreduce/mapreduce.py
@@ -1,98 +1,104 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
-import itertools
 
 class MapReduce(MRJob):
 
-    # In this function, we specify a customized order
-    # of steps to be performed. This is because we will
-    # need to gather the edge count information of each
-    # vertex in a final reducer.
+    # In this function, we specify a customized order of steps to be
+    # performed. This is because we will need to gather the edge
+    # count information of each vertex in a final reducer.
     def steps(self):
         return [
             MRStep(mapper=self.mapper_find_neighbors,
                    reducer=self.forward_neighbors),
             MRStep(mapper=self.map_two,
                    reducer=self.reduce_two),
-            MRStep(reducer=self.reduce_three)
+            # We had some issues collecting the count in the second reducer,
+            # so we performed a third step with a reducer collecting the sum
+            # of triangle counts.
+            MRStep(reducer=self.sum_values)
         ]
 
-    # Finds all vertices' neighbors
+    # Finds all vertices' neighbors.
     def mapper_find_neighbors(self, _, line):
         vertices = line.split()
         u, v = vertices[0], vertices[1]
         yield u, v
         yield v, u
 
+    # As we want the mapper in step two to receive all neighbors per
+    # vertex at a time, we will have to accumulate those in a reducer
+    # before directing them.
     def forward_neighbors(self, v, neighbors):
-        a = [x for x in neighbors]
-        #print(v, ' <> ', a)
-        yield v, a
+        yield v, [x for x in neighbors]
 
-        #for x in neighbors:
-            #yield v, x
 
-    # Now this mapper will match the expected input as the lecture
-    # explained.
+    # This mapper receives a vertex v and all neighbors of v. With this information,
+    # we can combine each element, x, of the neighbor-lists (generator), with v. This forms
+    # tuples, that we can associate to the neighbor-lists. By ordering these
+    # tuples in terms of (v,x) when v < x and (x,v) when otherwise, we will get
+    # x's neighbor-lists appended to the same (v,x) tuple - indicating that
+    # they will hold both v's and x's neighbor-lists. Thereby, in the later step, we can
+    # compare the two-neighbor vertices' common neighbors - which forms triangle-
+    # relationships.
     def map_two(self, v, node_pair):
 
         neighbors = [x for x in node_pair]
 
-        # Ensures that the two-neighboring vertices are always sorted.
+        # Ensures that the two-neighboring vertices tuple always is sorted.
         def f(a,b):
             return (a, b) if a < b else (b, a)
 
         return ((f(v, x), neighbors) for x in neighbors)
 
-    # Now we need to find vertices that two neighbors has
-    # in common
+    # Now we need to find neighbor vertices that two-neighbor vertices has
+    # in common.
     def reduce_two(self, k, values):
-
+        # Store the two-neighbor vertices in different variables.
         u, v = k[0], k[1]
-        def remDup(the_list):
-            b = []
-            [b.append(uniq) for uniq in the_list if uniq not in b]
-            return b
 
-        lst = [x for x in values]#remDup(values)
+        # Removes duplicates from a list, this is performed in order to
+        # discard any additional neighboring lists that appears when
+        # two-neighboring nodes have more than one edge between each other.
+        def remDup(collection):
+            to_return = []
+            [to_return.append(uniq) for uniq in collection if uniq not in to_return]
+            return to_return
+        values = remDup(values)
 
-        #lst = [x for x in values]
+        # c represents the two-neighbor vertices, which should not be included
+        # among the common neighbors set.
+        c = set([u,v])
+        #c.add(u)
+        #c.add(v)
 
-        #print(k, ' <> ', lst)
-        c = set()
-        c.add(u)
-        c.add(v)
-        #a = set(lst[0])
-        #b = set()
-        length = 0#len(a - c)
+        #length = 0
 
-        while len(lst) > 1:
-            a = set(lst[0])
-            b = set(lst[len(lst)-1])
-            length += len((a & b) - c)
-            del lst[0]
-            del lst[len(lst)-1]
+        #if len(lst) > 1:
+        a, b = set(values[0]), set(values[1])
 
+        #print(k, ' > ', values, ' ----> ', (a & b) - c)
 
-        '''if len(lst) > 1:
-            b = set(lst[1])
+        # Now we find all neighbors that u and v has in common (again, excluding
+        # u and v themselves by performing set difference with c).
+        tmp_neighbors = (a & b) - c
 
-        print(k, ' > ', lst, ' ----> ', (a & b) - c)
+        # In order to avoid adding duplicate triangles, we discard any neighbor, w,
+        # where w < u or w < v.
+        neighbors = set()
+        while tmp_neighbors:
+            w = tmp_neighbors.pop()
+            if w > u and w > v:
+                neighbors.add(w)
 
-        length = len((a & b) - c)'''
-        yield None, length / 2
-
+        # The amount of new neighbors will indicate the amount
+        # of new triangle counts to be emitted.
+        yield None, len(neighbors)
 
 
 
-    def reduce_three(self, _, values):
-       # print([x for x in values])
+    # The third reducer sums up the triangle counts.
+    def sum_values(self, _, values):
         yield 'Triangle count: ', sum(values)
-
-        #yield 1,1
-        #string = "Triangle in sequence: ", k, " ? "
-        #str_b = [v for v in values] #any(v is not None for v in values)
-        #yield string, str_b #v#sum(1 if v is not None else 0 for v in values)
 
 if __name__ == '__main__':
     MapReduce.run()
