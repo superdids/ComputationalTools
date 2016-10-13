5dce5599e4d03a7cde5e55bbb2e1a4bdeda8dd78
superdids <didrik.aubert@gmail.com>
2016-10-13 08:35:09 +0200

    Exercise 7.3 is maybe complete, not tested on the actual exercise yet.

--- mapreduce/mapreduce.py
+++ mapreduce/mapreduce.py
@@ -9,41 +9,43 @@ class MapReduce(MRJob):
     # vertex in a final reducer.
     def steps(self):
         return [
-            MRStep(mapper=self.mapper,
-                   reducer=self.reducer_vertices_edge_count),
-            MRStep(reducer=self.reducer_every_vertex_even)
+            MRStep(mapper=self.mapper_find_neighbors,
+                   reducer=self.reduce_length_two_paths),
+            MRStep(mapper=self.map_two,
+                   reducer=self.reduce_two)
         ]
 
-    # In the mapper, we read each line and extract a pair of
-    # vertices. We yield each vertex as a key and 1 as the
-    # value (edge count), so that we accumulate the number of
-    # edges for each vertex. This is because the same keys are
-    # heading to the same reducer.
-    def mapper(self, _, line):
+    '''
+    We avoid cycles by 'directing' the vertices' neighborhood.
+    '''
+    def mapper_find_neighbors(self, _, line):
         vertices = line.split()
-        yield vertices[0], 1
-        yield vertices[1], 1
+        u, v = vertices[1], vertices[0]
+        #print(u, ' ', v)
+        yield u, v
 
-    # When the values enter the first reduction step, we want
-    # to send a single list to the second reduction step - as
-    # we rely on a single output in this exercise. A single list
-    # can be passed to another reduction function by simply
-    # associating all yielded values to a single key - for instance
-    # 'result'. We basically yield values True and False, which
-    # indicates that a specific vertex has an even amount of edges
-    # or not.
-    def reducer_vertices_edge_count(self, vertex, values):
-        yield 'result', (sum(values) % 2 == 0, vertex)
+    def reduce_length_two_paths(self, v, neighbors):
+        for u in neighbors:
+            for w in neighbors:
+                if u is not w and u is not v and w is not v:
+                    print(v, ': ', '(', u, ' ', w, ')')
+                    yield v, (u,w)
 
-    # When the values enter the second reduction step, the function
-    # receives only one key - and consequently only one generator.
-    # By checking if every vertex has an even amount of edges,
-    # we know whether there is a euler path or not in the graph. We
-    # are not interested in the vertex names in this case, which is
-    # why the underscore in (x,_) is used.
-    def reducer_every_vertex_even(self, _, values):
-        yield "Euler tour? ", all(x for (x,_) in values)
+    '''
+    if Input of type #v; (u, w)$ then
+    emit #(u, w); v$
+    if Input of type #(u, v); ∅$ then
+    emit #(u, v); $$
+    '''
+    def map_two(self, v, node_pair):
+        if node_pair is None:
+            yield (v[0], v[1]), None
+        else:
+            yield None, 1#((node_pair[0], node_pair[1]), v)
 
+    def reduce_two(self, v, values):
+        if v is None:
+            yield "Triangles: ", sum(values)#len(values)#1#sum(1 if x is not None else 0 for x in values)
 
 if __name__ == '__main__':
     MapReduce.run()
