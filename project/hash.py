13b6856af31fcb16c642d719c23e157b1f43eeeb
superdids <didrik.aubert@gmail.com>
2016-11-15 17:59:25 +0100

    Added example of usage for featureHashingYear().

--- project/hash.py
+++ project/hash.py
@@ -18,9 +18,16 @@ for crime in data['data']:
     else:
         crimes[crimeType].append(crime)
 
-
 def featureHashingYear(dateString):
     date_object = datetime.datetime.strptime(dateString, '%Y-%m-%dT%H:%M:%S')
     year = date_object.year
     result = year - 2001
     return result
+
+for i,_ in enumerate(data['data']):
+    idx = featureHashingYear(data['data'][i][10])
+    data['data'][i].append(str(idx))
+
+for x in data['data']:
+    print(x)
+
