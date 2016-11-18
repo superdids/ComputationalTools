50e868464cb1c2053c88b987cc85f07d006d3b24
Galin Kostov <gako@esmiley.dk>
2016-11-18 16:15:13 +0100

    Load the JSON with ijson

--- project/parse_json_to_text.py
+++ project/parse_json_to_text.py
@@ -1,9 +1,10 @@
 import json
+import ijson
 
 with open('testData.json') as f:
-    dataJSON = json.load(f)
+    dataJSON = ijson.items(f, 'data.item')
     dataText = open('data.txt', 'w')
-    for crime in dataJSON['data']:
+    for crime in dataJSON:
         crimeText = json.dumps(crime)
         dataText.write(crimeText + '\n')
 
