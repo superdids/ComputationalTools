641d0d0681445bee4832fc6b2c7e453a19ade6a5
Galin Kostov <gako@esmiley.dk>
2016-11-30 10:45:25 +0100

    Add install modules script

---
+++ project/install.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+module load numpy/1.9.2-python-2.7.3-openblas-0.2.14 && module load scipy/scipy-0.15.1-python-2.7.3 && module load pandas/0.16.2-python.2.7.3 && pip install --user  scikit-learn && pip install --user  mrjob
