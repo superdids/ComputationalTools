7fe0bea26f287202aaf327031a239a4f8c55def2
Galin Kostov <gako@esmiley.dk>
2016-11-30 10:26:17 +0100

    Cluster crimes by subtypes and by areas and parse to JSON

---
+++ project/mrSubtypesCrimes.py
@@ -0,0 +1,57 @@
+import datetime
+import time
+from mrjob.job import MRJob
+import json
+
+valid_crime_types = {
+    'robbery',
+    'stalking',
+    'prostitution',
+    'narcotics',
+    'motor vehicle theft',
+    'kidnapping',
+    'theft',
+    'assault',
+    'battery'
+}
+
+
+def type_hashing(entry):
+    return entry[13].lower()
+
+
+def subtype_hashing(entry):
+    return entry[14].lower()
+
+
+def area_hashing(entry):
+    return entry[15].lower()
+
+
+class ReduceData(MRJob):
+    # Generates a (k,v) pair for each occurrence of a word
+    # in a text file.
+    def mapper(self, _, line):
+        lineJSON = json.loads(line)
+        crimeType = type_hashing(lineJSON)
+        crimeSubtype = subtype_hashing(lineJSON)
+        crimeArea = area_hashing(lineJSON)
+
+        if crimeType in valid_crime_types:
+            yield (crimeType, crimeSubtype, crimeArea), 1
+
+    # The reduction step now performs the sum function on the values list,
+    # and returns a single value as result.
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+
+if __name__ == '__main__':
+    # START the timer
+    start = time.time()
+
+    ReduceData.run()
+
+    # END the timer
+    end = time.time()
+    # print('processing time: ', end - start)
---
+++ project/parseCrimesSubtypesAreasToJSON.py
@@ -0,0 +1,26 @@
+import sys
+import json
+
+data = sys.stdin.read().split('\n')
+
+distinctCrimes = dict()
+
+for line in data:
+    if len(line) == 0:
+        continue
+
+    crimeNumbers = int(line[line.find('\t') + 1:len(line)])
+    crimeDataArray = line[line.find('[') + 1:line.find(']')].split('", ')
+    crime = crimeDataArray[0].replace('"', '')
+    crimeSubtype = crimeDataArray[1].replace('"', '')
+    crimeArea = crimeDataArray[2].replace('"', '')
+
+    if crime not in distinctCrimes:
+        distinctCrimes[crime] = dict()
+    if crimeSubtype not in distinctCrimes[crime]:
+        distinctCrimes[crime][crimeSubtype] = dict()
+    if crimeArea not in distinctCrimes[crime][crimeSubtype]:
+        distinctCrimes[crime][crimeSubtype][crimeArea] = crimeNumbers
+
+with open('dataCrimesSubtypesAreas.json', 'w') as jsonFile:
+    json.dump(distinctCrimes, jsonFile)
